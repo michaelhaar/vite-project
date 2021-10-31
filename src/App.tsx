f3f9f0294d70d88e94707f814ecfc6f791e6b7c2
Michael Haar <michael.haar92@gmail.com>
2021-10-31 10:21:25 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+/* src/App.jsx */
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="font-bold text-2xl text-blue-900">
+        React TS and Tailwind with Vitejs!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
